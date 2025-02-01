756b80e8120d2ad65cc17c9ca35cfeb9c396cf8d
jacobrgithub <128652920+jacobrgithub@users.noreply.github.com>
2025-01-31 19:16:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+var heading = 360;
 
+updateScreen();
+
+onEvent("rightButton", "click", function( ) {
+  if (heading > 359) {
+    heading = heading % 359;
+  } else {
+    heading = heading + 1;
+  }
+  setStyle("image1", "transform: rotate(" + (-heading) + "deg);");
+  updateScreen();
+});
+
+onEvent("leftButton", "click", function( ) {
+  if (heading < 2) {
+    heading = 360;
+  } else {
+    heading = heading-1;
+  }
+  setStyle("image1", "transform: rotate(" + (-heading) + "deg);");
+  updateScreen();
+});
+
+function updateScreen() {
+  setProperty("headingOutput", "text", heading + "°");
+  setStyle("image1", "transform: rotate(" + (-heading) + "deg);");
+}
